111e7c34df24a686e499975d9918cb768b8c1f77
Vinicius Koncicoski <viniciuskoncicoski@gmail.com>
2025-02-08 18:35:24 -0300

    feat: create form contact and contacts components

---
+++ resources/views/livewire/form-contact.blade.php
@@ -0,0 +1,24 @@
+<div>
+    <form>
+
+        <div class="mb-3">
+            <label for="name">Name</label>
+            <input type="text" class="form-control" id="name">
+        </div>
+        
+        <div class="mb-3">
+            <label for="email">Email</label>
+            <input type="text" class="form-control" id="email">
+        </div>
+        
+        <div class="mb-3">
+            <label for="phone">Phone</label>
+            <input type="text" class="form-control" id="phone">
+        </div>
+        
+        <div class="text-env">
+            <button class="btn btn-sencondary px-5">Save</button>
+        </div>
+        
+    </form>
+</div>
