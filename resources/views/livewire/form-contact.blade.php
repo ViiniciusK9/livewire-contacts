ebf09ab05d31f661989f36839cf4622d6e4ab8e5
Vinicius Koncicoski <viniciuskoncicoski@gmail.com>
2025-02-08 18:54:26 -0300

    feat: contact form validation

--- resources/views/livewire/form-contact.blade.php
+++ resources/views/livewire/form-contact.blade.php
@@ -1,19 +1,28 @@
 <div>
-    <form>
+    <form wire:submit="newContact">
 
         <div class="mb-3">
             <label for="name">Name</label>
-            <input type="text" class="form-control" id="name">
+            <input type="text" class="form-control" id="name" wire:model="name">
+            @error('name')
+                <span class="text-danger">{{ $message }}</span> 
+            @enderror
         </div>
         
         <div class="mb-3">
             <label for="email">Email</label>
-            <input type="text" class="form-control" id="email">
+            <input type="text" class="form-control" id="email" wire:model="email">
+            @error('email')
+                <span class="text-danger">{{ $message }}</span> 
+            @enderror
         </div>
         
         <div class="mb-3">
             <label for="phone">Phone</label>
-            <input type="text" class="form-control" id="phone">
+            <input type="text" class="form-control" id="phone" wire:model="phone">
+            @error('phone')
+                <span class="text-danger">{{ $message }}</span> 
+            @enderror
         </div>
         
         <div class="text-end">
